fc2d342c5e28577779a9aec3cad6d4a54d2a7fb5
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-05-20 01:42:48 -0500

    font stylings

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,7 +4,7 @@ const About = () => {
 return (
     <section>
         <h2>About</h2>
-        <p className="bio">Hello, I'm Jason W. Smith, a DIY drummer dad living the dream in beautiful Nashville, TN.  Quickly becoming a budding web developer learning to code one application at a time.  Please feel free to take a look around!</p>
+        <p className="bio">Hello, I'm Jason.  I'm a budding web developer utilizing a wide array of skillsets developed through years of freelance work within the music industry.  Recent certificate recipient from Vanderbilt University’s full stack coding bootcamp including skills in the entire MERN stack, JavaScript, HTML and CSS.  Developed 3 projects in a group setting during bootcamp, including a MERN app, Neighbor:Good that allows for users to request or fulfill jobs as posted within the app.  Passionate about problem solving in a group context and leading groups to be as effective as possible by assign appropriate tasks base on developer strengths.  I’m excited to use my past work skills to improve the world of web development.</p>
     </section>
     )
 }
