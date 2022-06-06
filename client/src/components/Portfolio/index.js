616a017b44f99aefebbee2503075322a7653549d
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-06-06 02:33:56 -0500

    added styled components

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Project from "../Project"
 
+
 const Portfolio = () => {
   const projects = [
     
--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import { VscGithub } from 'react-icons/vsc'
+import styled from 'styled-components';
 
 const Project = ({project}) => {
     const {name, repo, link, image } = project
   return (
-        <div class="project" key={name}>
+        <ProjectStyled class="project" key={name}>
             <h3>{name}</h3>
             <img src={image}/>
-            <a href={repo} target="_blank"><VscGithub size="2em" /></a>
-            <a href={link} target="_blank">Deployed Application</a>
-        </div>
+            <div id="links">
+              <a href={repo} target="_blank"><VscGithub size="2em" /></a>
+              <a href={link} target="_blank">Deployed Application link</a>
+            </div>
+        </ProjectStyled>
     
   )
-}
+};
+
+const ProjectStyled = styled.section`
+  display:flex;
+  flex-direction: column;
+  margin: 10px;
+
+  #links {
+    padding: 1rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
+  }
+`;
 
 export default Project
